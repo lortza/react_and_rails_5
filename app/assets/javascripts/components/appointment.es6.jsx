4800638f1895f147209a7eb625ca27975454cd05
anne <richardson.ae@gmail.com>
2018-04-26 20:01:16 -0500

    Uses deconstruction with static components

--- app/assets/javascripts/components/appointment.es6.jsx
+++ app/assets/javascripts/components/appointment.es6.jsx
@@ -1,15 +1,14 @@
 // these appointments are being rendered by the AppointmentsList component
 
-class Appointment extends React.Component {
-  render () {
-    return (
-      // set up how each appointment will be rendered
-      <div className="card card-body mt-2">
-        <h3>{this.props.appointment.title}</h3>
-        <p>{formatDate(this.props.appointment.start_time)}</p>
-      </div>
-    );
-  }
+// Use destructuring to convert props to appointment
+const Appointment = ({appointment}) => {
+  return (
+    // set up how each appointment will be rendered
+    <div className="card card-body mt-2">
+      <h3>{appointment.title}</h3>
+      <p>{formatDate(appointment.start_time)}</p>
+    </div>
+  );
 }
 
 
--- app/assets/javascripts/components/appointments_list.es6.jsx
+++ app/assets/javascripts/components/appointments_list.es6.jsx
@@ -1,17 +1,13 @@
-class AppointmentsList extends React.Component {
-  render () {
-    return (
-      <div id="appointments-list">
-        {this.props.appointments.map(function(appt){
-          return(
-            // pass the appt data to the Appointment component
-            // you also need to set a unique key for each mapped component
-            <Appointment key={appt.id} appointment={appt}/>
-          )
-        })}
-      </div>
-    )// return
-  }
+const AppointmentsList = ({appointments}) => {
+  return (
+    <div id="appointments-list">
+      {appointments.map(function(appt){
+        return(
+          // pass the appt data to the Appointment component
+          // you also need to set a unique key for each mapped component
+          <Appointment key={appt.id} appointment={appt}/>
+        )
+      })}
+    </div>
+  )// return
 }
-
-
